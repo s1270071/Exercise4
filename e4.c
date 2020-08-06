bc6afd30e2f3ab6ba04188cd276b15ac4d2907a5
Shunsuke Koeda <s1270071@u-aizu.ac.jp>
2020-08-06 21:03:09 +0900

    HoT: Initial revision

---
+++ e4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int cnt=0;
+    int i;
+    int ran;
+    
+    srand((int)time(NULL));
+    printf("Tosiing a coin...\n");
+    for(i=0;i<3;i++){
+        printf("Round %d: ",i+1);
+        ran = rand()%2;
+        if(ran==0){
+            printf("Heads\n");
+            cnt++;
+        }
+        else if(ran==1) printf("Tails\n");
+    }
+    printf("Heads: %d, Tails: %d\n",cnt,3-cnt);
+    return 0;
+}
